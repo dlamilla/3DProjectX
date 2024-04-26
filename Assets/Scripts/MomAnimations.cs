5114f4ae483c589a4f0b2d50a0d5598bd2f72506
DLamilla20 <lamilla.tenazoa@gmail.com>
2024-04-26 11:50:57 -0500

    v0.1.1

---
+++ Assets/Scripts/MomAnimations.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomAnimations : MonoBehaviour
+{
+    [SerializeField] private float _time;
+    private Animator _anim;
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("EmpezarAnimaciones", 3f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void EmpezarAnimaciones()
+    {
+        StartCoroutine(Animaciones());
+    }
+
+    IEnumerator Animaciones()
+    {
+        yield return new WaitForSeconds(_time);
+        _anim.SetTrigger("inicio");
+    }
+}
