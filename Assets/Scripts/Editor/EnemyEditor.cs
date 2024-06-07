b407ee7569875d57a5da888cb441f2129a388ae9
DLamilla20 <lamilla.tenazoa@gmail.com>
2024-06-07 01:28:07 -0500

    FOVenemyComplete

---
+++ Assets/Scripts/Editor/EnemyEditor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using TMPro;
+
+[CustomEditor(typeof(FOVenemy))]
+public class EnemyEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        FOVenemy _enem = (FOVenemy)target;
+
+        Color c = Color.green;
+        if (_enem.alertStage == AlertStage.Intrigued)
+        {
+            c = Color.Lerp(Color.green, Color.red, _enem.alertLevel / 100f);
+        }
+        else if (_enem.alertStage == AlertStage.Alerted)
+        {
+            c = Color.red;
+        }
+
+        Handles.color = new Color(c.r, c.g, c.b, 0.3f);
+        Handles.DrawSolidArc(_enem.transform.position, _enem.transform.up, Quaternion.AngleAxis(-_enem.fovAngle / 2f, _enem.transform.up) * _enem.transform.forward, _enem.fovAngle, _enem.fov);
+        Handles.color = c;
+        _enem.fov = Handles.ScaleValueHandle(_enem.fov, _enem.transform.position + _enem.transform.forward * _enem.fov, _enem.transform.rotation, 3, Handles.SphereHandleCap, 1);
+    }
+}
--- Assets/Scripts/FOVenemy.cs
+++ Assets/Scripts/FOVenemy.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
+public enum AlertStage
+{
+    Peaceful,
+    Intrigued,
+    Alerted
+}
 public class FOVenemy : MonoBehaviour
 {
-    [SerializeField] private float fov;
+    [SerializeField] public float fov;
+    [Range(0, 360)] public float fovAngle;
+    [SerializeField] public AlertStage alertStage;
+    [Range(0, 100)] public float alertLevel;
+    [SerializeField] private LayerMask _player;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +24,59 @@ public class FOVenemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool playerInFOV = false;
+        Collider[] targetsInFOV = Physics.OverlapSphere(transform.position, fov, _player);
+        foreach (Collider c in targetsInFOV)
+        {
+            if (c.CompareTag("Player"))
+            {
+                float signedAngle = Vector3.Angle(transform.forward, c.transform.position - transform.position);
+                if (Mathf.Abs(signedAngle) < fovAngle / 2)
+                {
+                    playerInFOV = true;
+                }
 
+                break;
+            }
+        }
+
+        UpdateAlertState(playerInFOV);
     }
-
-    private void OnDrawGizmos()
+    private void UpdateAlertState(bool playerInFOV)
     {
-        Handles.color = new Color(0, 1, 0, 0.3f);
-        Handles.DrawSolidDisc(transform.position, transform.up, fov);
+        switch (alertStage)
+        {
+            case AlertStage.Peaceful:
+                if (playerInFOV)
+                {
+                    alertStage = AlertStage.Intrigued;
+                }
+                break;
+            case AlertStage.Intrigued:
+                if (playerInFOV)
+                {
+                    alertLevel++;
+                    if (alertLevel >= 100)
+                    {
+                        alertStage = AlertStage.Alerted;
+                    }
+                }
+                else
+                {
+                    alertLevel--;
+                    if (alertLevel <= 0)
+                    {
+                        alertStage = AlertStage.Peaceful;
+                    }
+                }
+                break;
+            case AlertStage.Alerted:
+                if (!playerInFOV)
+                {
+                    alertStage = AlertStage.Intrigued;
+                }
+                break;
+        }
     }
+
 }
